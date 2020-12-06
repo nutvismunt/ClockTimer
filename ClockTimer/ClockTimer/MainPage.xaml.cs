c94e5f54bcb86be2e85ff9087625269aadca583d
nutvismunt <nutvismunt@gmail.com>
2020-12-07 00:25:55 +0300

    Добавьте файлы проекта.

---
+++ ClockTimer/ClockTimer/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using Xamarin.Forms;
+
+namespace ClockTimer
+{
+    public partial class MainPage : ContentPage
+    {
+        object container;
+        string timeLeft;
+        public MainPage()
+        {
+            InitializeComponent();
+
+         //   if (App.Current.Properties.TryGetValue("isTimer", out container))
+         //   {
+           //     timeLeft = Convert.ToDateTime(App.Current.Properties["isTimer"].ToString()).ToString("HH:mm:ss");
+          //  }
+
+
+            SetTimer();
+        }
+
+        private void SetTimer()
+        {    
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    date.Text = DateTime.Now.ToString("dd.MM.yyyy");
+                    clock.Text = DateTime.Now.ToString("HH:mm:ss");
+                  //  Ctimer.Text = !string.IsNullOrWhiteSpace(timeLeft)? Convert.ToDateTime(timeLeft).AddSeconds(-1).ToString("HH:mm:ss") : "Таймер";
+                 //   var form = Convert.ToDateTime(container.ToString());
+                 //  var time = form.AddSeconds(-1).ToString("HH:mm:ss");
+                    /*  if (App.Current.Properties.TryGetValue("isTimer", out container))
+                      {
+                          clock_Timer();
+                      }
+                      else Ctimer.Text = "Таймер";*/
+                });
+                return true;
+            });
+
+
+        }
+
+        private async void OnTimer(object sender, EventArgs e)
+        {
+            var timer = new Timer();
+            await Navigation.PushAsync(timer);
+        }
+
+
+        private void clock_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (App.Current.Properties.TryGetValue("isTimer", out container))
+            {
+
+
+                        if (container != null && container.ToString() != "00:00:00")
+                        {
+                           var b = Convert.ToDateTime(container.ToString()).AddSeconds(-1).ToString("HH:mm:ss");
+                            Ctimer.Text = b;
+                            App.Current.Properties.Remove("isTimer");
+                            App.Current.Properties.Add("isTimer", b);
+                            container = App.Current.Properties["isTimer"];
+                        }
+                        else if (container.ToString() == "00:00:00")
+                        {
+                            App.Current.Properties.Remove("isTimer");
+                            DisplayAlert("Таймер", "Таймер достиг 00:00:00", "ОК");
+                        }
+                        else Ctimer.Text = "Таймер";
+            }
+        }
+    }
+}
---
+++ ClockTimer/ClockTimer/Timer.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClockTimer
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Timer : ContentPage
+    {
+        public DateTime _time;
+        public bool isenabled;
+        public Timer()
+        {
+            InitializeComponent();
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(seconds.Text))
+                if (Convert.ToInt32(seconds.Text) > 60)
+                {
+                    string entryText = seconds.Text;
+                    seconds.TextChanged -= OnEntryTextChanged;
+                    seconds.Text = e.OldTextValue;
+                    seconds.TextChanged += OnEntryTextChanged;
+                }
+        }
+        private async void StartTimer(object sender, EventArgs e)
+        {
+            bool returnValue;
+            if (startStop.Text == "старт")
+            {
+                returnValue = true;
+                startStop.Text = "стоп";
+            }
+            else
+            {
+                startStop.Text = "старт";
+                returnValue = false;
+            }
+
+          //  StartingTimer(Convert.ToDateTime(timePicker.Time.ToString()).AddSeconds(Convert.ToInt32(seconds.Text)), returnValue);
+            var main = new MainPage();
+            _time = Convert.ToDateTime(timePicker.Time.ToString()).AddSeconds(Convert.ToInt32(seconds.Text));
+          //  main.BindingContext = (_time,false);
+            App.Current.Properties.Add("isTimer",_time);
+            await Navigation.PopAsync();
+
+        }
+      /*  public async void StartingTimer(DateTime time, bool returnValue)
+        {
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    _time = time.AddSeconds(-1);
+                });
+                return returnValue; // True = Repeat again, False = Stop the timer
+            });
+        }*/
+    }
+}
